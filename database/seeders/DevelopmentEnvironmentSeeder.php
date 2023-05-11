ffe7d660850530471f2fea91dea005d1f820d73a
Jorge Murta <jorge@jorgemurta.com>
2023-05-11 23:13:34 +0100

    add testing user

---
+++ database/seeders/DevelopmentEnvironmentSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DevelopmentEnvironmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->createUser();
+    }
+
+    public function createUser(): User
+    {
+        return User::factory()->withPersonalTeam()->create([
+            'name' => 'John Doe',
+            'email' => 'user@example.com',
+        ]);
+    }
+}
